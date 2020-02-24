6f758743efc78bb805c6674bb7cf36ab7e9cfd85
bini <binyamasrat@gmail.com>
2020-02-24 23:33:09 +0300

    images added to card

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './17.2 card-list.styles.css'
+import {Card} from "../card/card.component";
 
 export const CardList = props => (
     <div className="card-list">
-        {props.cardMon.map(monst => <div key={monst.id}><h2>id: {monst.id}</h2><h1>{monst.name}</h1>
-        </div>)}
+        {props.cardMon.map(monst => <Card key={monst.id} monst = {monst}/>)}
     </div>
 );
