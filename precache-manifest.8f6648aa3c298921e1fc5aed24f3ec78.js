c725c6d0947de8bbb1f0e16221314a5e6e627993
bini219 <123456789>
2020-03-04 11:04:34 +0300

    Updates

---
+++ precache-manifest.8f6648aa3c298921e1fc5aed24f3ec78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f528ffaf31bbf7622c374543f4459c17",
+    "url": "/joSucks/index.html"
+  },
+  {
+    "revision": "bb268834d982e5fe0aae",
+    "url": "/joSucks/static/css/main.fc9f9745.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/joSucks/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/joSucks/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb268834d982e5fe0aae",
+    "url": "/joSucks/static/js/main.0b064ab0.chunk.js"
+  },
+  {
+    "revision": "0af5c66e585d871e2d06",
+    "url": "/joSucks/static/js/runtime-main.33eb1b81.js"
+  }
+]);
