3f1fcd2234dcd83d94a5d317cba8b0d7e73b5569
bini219 <123456789>
2020-03-05 11:09:47 +0300

    ** button component added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import './App.css';
 import {CardList} from "./components/card-list/card-list.component";
 import {SearchBox} from "./components/search-box/search-box.component";
+import {ButtonTest} from "./components/buttonTest/buttonTest.component";
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
             monsters: [],
-            searchField: ""
+            searchField: "",
+            lidisAge: 12
         };
     }
 
@@ -22,6 +24,9 @@ class App extends Component {
     handleChange = (e) => {
         this.setState({searchField: e.target.value});
     }
+    chemer = () =>{
+       this.setState( {lidisAge : this.state.lidisAge + 1});
+    }
 
     render() {
         const {monsters, searchField} = this.state;
@@ -33,6 +38,8 @@ class App extends Component {
             <div className="App">
                 <h1>Yohannes Begashaw is the real MONSTER here</h1>
                 <h2>NO One! Absolutely NO ONE! Wants to be like him!!!!!</h2>
+                <h1>{this.state.lidisAge}</h1>
+               <ButtonTest name="chemer fekes chemer" chemer={this.chemer}/>
                 <SearchBox placeholder={"Search Monster"} handleChange={this.handleChange}/>
                 <CardList monsters={filteredMonsters}/>
             </div>
---
+++ src/components/buttonTest/buttonTest.component.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const ButtonTest = ({name,chemer}) =>(
+    <button onClick={chemer}>{name}</button>
+)
